5d90f26d66dcc78e90a17a5357c4083c62f25672
邰爱强 <50889574@qq.com>
2019-02-12 21:22:38 +0800

    add mapping

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,20 @@
+//映射关系
+const map = {
+  main:{
+    ysj: '颜色级',
+    cd: '长度',
+    ql: '强力',
+    mz: '马值',
+    cz: '长整',
+    hc: '回潮',
+    hz: '含杂'
+  }
+};
 const config = {
   dev: true,
   host: '',
-  version: '0.0.1'
+  version: '0.0.1',
+  map
 };
 
 module.exports = config;
