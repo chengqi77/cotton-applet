fd496ac9613eb876c43c2b391680828dae893f61
邰爱强 <50889574@qq.com>
2019-02-13 22:06:22 +0800

    完成我的供需-我的需求页面

---
+++ pages/my-demand/index.js
@@ -0,0 +1,98 @@
+// pages/my-demand/index.js
+const config = require('../../utils/config.js');
+const item = {
+  id: '562781322',
+
+  ysj: '21+',
+  cd: '12',
+  ql: 21.2,
+  mz: 1,
+  cz: '0.0',
+  hc: '0.0',
+  hz: '0.0',
+  jg: '<15003',
+
+  shd: '盐城',
+  mj: '盐城捷多纺织品有限公司',
+  zwjhsj: '2019-01-01',
+  cgjs: '200d吨',
+};
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    activeKey: 'demand',
+    list: [item, item, item, item, item],
+    map: config.map.main,
+    itemKeyList: ['ysj', 'cd', 'ql', 'mz', 'cz', 'hz', 'jg'],
+    itemDescList: ['mj', 'cgjs', 'shd', 'zwjhsj'],
+  },
+  handleTabChange(e) {
+    const { detail: { key } } = e;
+    this.setData({
+      activeKey: key
+    });
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    wx.showNavigationBarLoading();
+    setTimeout(()=>{
+      wx.hideNavigationBarLoading();
+      wx.stopPullDownRefresh();
+    }, 1000);
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
